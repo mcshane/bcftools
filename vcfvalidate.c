91d34493c6818cf34d94008260fa63fd6cb68091
Shane McCarthy <sm15@sanger.ac.uk>
2014-08-19 14:18:36 +0100

    vcfvalidate progress commit
    * update whitespace and license to match changes in main code

--- vcfvalidate.c
+++ vcfvalidate.c
@@ -1,27 +1,26 @@
-/* The MIT License
+/*  vcfvalidate.c -- validate subcommand.
 
-   Copyright (c) 2014 Genome Research Ltd.
-   Authors:  see http://github.com/samtools/bcftools/blob/master/AUTHORS
+    Copyright (C) 2014 Genome Research Ltd.
 
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
+    Author: Shane McCarthy <sm15@sanger.ac.uk>
 
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-   THE SOFTWARE.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
- */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.  */
 
 #include <unistd.h>
 #include <getopt.h>
@@ -43,6 +42,7 @@ args_t;
 
 static void destroy_data(args_t *args)
 {
+    if (args->faidx) fai_destroy(args->faidx);
     free(args->info_ac);
     free(args->fmt_ac);
 }
@@ -51,7 +51,7 @@ int vcf_validate(args_t *args, bcf1_t *line)
 {
     int i;
     // check for parse errors
-    if ( line->errcode ) 
+    if ( line->errcode )
     {
         if (line->errcode&BCF_ERR_CTG_UNDEF)
             fprintf(stderr,"Contig not defined in header found at %s:%d.\n", bcf_seqname(args->hdr,line), line->pos+1);
@@ -61,19 +61,19 @@ int vcf_validate(args_t *args, bcf1_t *line)
             fprintf(stderr,"Number of columns does not match number of samples in the header at %s:%d (%d vs %d).\n", bcf_seqname(args->hdr,line), line->pos+1, line->n_sample, bcf_hdr_nsamples(args->hdr));
         return -1;
     }
-    
+
     // check INFO field lengths
     for (i=0; i<line->n_info; i++)
     {
         bcf_unpack(line, BCF_UN_INFO);
         bcf_info_t *info = &line->d.info[i];
         int vlen = bcf_hdr_id2length(args->hdr,BCF_HL_INFO,info->key);
-        
+
         int type = bcf_hdr_id2type(args->hdr,BCF_HL_INFO,info->key);
         if ( type==BCF_HT_FLAG ) {
             if (info->len>0) {
                 fprintf(stderr, "INFO/%s is Type=Flag in header, but found %d values at: %s:%d\n", 
-                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), info->len, bcf_seqname(args->hdr,line), line->pos+1);        
+                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), info->len, bcf_seqname(args->hdr,line), line->pos+1);
                 return -1;
             }
             continue;
@@ -102,23 +102,23 @@ int vcf_validate(args_t *args, bcf1_t *line)
             }
             nfound = svlen+1;
         }
-        
+
         if ( vlen==BCF_VL_A && nfound!=line->n_allele-1 )
         {
-            fprintf(stderr, "INFO/%s is Number=A in header. Unexpected number of fields at: %s:%d .. %d vs %d\n", 
-                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, line->n_allele-1);        
+            fprintf(stderr, "INFO/%s is Number=A in header. Expected %d, but found %d at: %s:%d\n", 
+                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), line->n_allele-1, nfound, bcf_seqname(args->hdr,line), line->pos+1);
             return -1;
         }
         else if ( vlen==BCF_VL_R && nfound!=line->n_allele )
         {
-            fprintf(stderr, "INFO/%s is Number=R in header. Unexpected number of fields at: %s:%d .. %d vs %d\n", 
-                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, line->n_allele);        
+            fprintf(stderr, "INFO/%s is Number=R in header. Expected %d, but found %d at: %s:%d\n", 
+                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), line->n_allele, nfound, bcf_seqname(args->hdr,line), line->pos+1);
             return -1;
         }
         else if ( vlen==BCF_VL_FIXED && nfound!=bcf_hdr_id2number(args->hdr,BCF_HL_INFO,info->key) )
         {
-            fprintf(stderr, "INFO/%s is Number=%d in header, but %d fields found at: %s:%d\n", 
-                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_hdr_id2number(args->hdr,BCF_HL_INFO,info->key), nfound, bcf_seqname(args->hdr,line), line->pos+1);        
+            fprintf(stderr, "INFO/%s is Number=%d in header, but %d found at: %s:%d\n", 
+                bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_hdr_id2number(args->hdr,BCF_HL_INFO,info->key), nfound, bcf_seqname(args->hdr,line), line->pos+1);
             return -1;
         }
         else if ( vlen==BCF_VL_G )
@@ -126,9 +126,9 @@ int vcf_validate(args_t *args, bcf1_t *line)
             // TODO: guess ploidy based on length of GT -> fill in expected number for G
             int ng = line->n_allele*(line->n_allele + 1)/2;
             if (nfound!=ng && nfound!=line->n_allele) { // diploid or haploid for the moment
-                fprintf(stderr, "INFO/%s is Number=G in header. Unexpected number of fields at: %s:%d .. %d vs %d\n", 
-                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, ng);        
-                return -1;                
+                fprintf(stderr, "INFO/%s is Number=G in header. Expected %d, but found %d at: %s:%d\n", 
+                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), ng, nfound, bcf_seqname(args->hdr,line), line->pos+1);
+                return -1;
             }
         }
     }
@@ -138,18 +138,93 @@ int vcf_validate(args_t *args, bcf1_t *line)
     {
         bcf_unpack(line, BCF_UN_FMT);
         bcf_fmt_t *fmt = &line->d.fmt[i];
-        // int vlen = bcf_hdr_id2length(args->hdr,BCF_HL_FMT,fmt->id);
+        int is_gt = strcmp(bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id),"GT")==0;
+        int vlen = bcf_hdr_id2length(args->hdr,BCF_HL_FMT,fmt->id);
 
         int type = bcf_hdr_id2type(args->hdr,BCF_HL_FMT,fmt->id);
         if ( type==BCF_HT_FLAG ) {
             if (fmt->n>0) {
                 fprintf(stderr, "FORMAT/%s is Type=Flag in header, but found %d values at: %s:%d\n", 
-                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), fmt->n, bcf_seqname(args->hdr,line), line->pos+1);        
+                    bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), fmt->n, bcf_seqname(args->hdr,line), line->pos+1);
                 return -1;
             }
             continue;
         }
-        // TODO: the rest of FORMAT
+        int size = 1;
+        if ( type==BCF_HT_REAL || type==BCF_HT_INT ) size = 4;
+
+        uint8_t *dat = NULL;
+        int mdat = 0, mdat_bytes = 0, nfound;
+        mdat = mdat_bytes / size;
+        if (is_gt)
+            nfound = bcf_get_genotypes(args->hdr, line, (void**)&dat, &mdat);
+        else
+            nfound = bcf_get_format_values(args->hdr, line, bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), (void**)&dat, &mdat, type);
+
+        mdat_bytes = mdat * size;
+        if ( nfound<0 )
+        { 
+            fprintf(stderr,"Could not access FORMAT/%s at %s:%d [%d]\n", 
+                bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), bcf_seqname(args->hdr,line), line->pos+1, nfound);
+            return -1;
+        }
+        if ( !is_gt && type==BCF_HT_STR )
+        {
+            int j, svlen = 0;
+            char *str = (char*) dat;
+            for (j=0; j<nfound; j++) {
+                if (str[j]==',')
+                    svlen++;
+            }
+            nfound = svlen+1;
+        }
+
+        if (is_gt) continue;
+
+        if ( vlen==BCF_VL_FIXED && nfound!=line->n_sample*bcf_hdr_id2number(args->hdr,BCF_HL_FMT,fmt->id) )
+        {
+            fprintf(stderr, "FORMAT/%s is Number=%d in header, but %d fields found at: %s:%d\n",
+                bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), line->n_sample*bcf_hdr_id2number(args->hdr,BCF_HL_FMT,fmt->id), nfound, bcf_seqname(args->hdr,line), line->pos+1);
+            return -1;
+        }
+        // else if ( vlen==BCF_VL_G )
+        // {
+        //     // TODO: guess ploidy based on length of GT -> fill in expected number for G
+        //     int ng = line->n_sample*line->n_allele*(line->n_allele + 1)/2;
+        //     if (nfound!=ng && nfound!=line->n_sample*line->n_allele) { // diploid or haploid for the moment
+        //         fprintf(stderr, "FORMAT/%s is Number=G in header, but %d fields found at: %s:%d. Expected %d for diploid or %d for haploid (other ploidy not yet supported)\n",
+        //             bcf_hdr_int2id(args->hdr,BCF_DT_ID,fmt->id), bcf_seqname(args->hdr,line), line->pos+1, nfound, ng, );
+        //         return -1;
+        //     }
+        // }
+        // if ( vlen==BCF_VL_A && nfound!=line->n_allele-1 )
+        // {
+        //     fprintf(stderr, "INFO/%s is Number=A in header. Unexpected number of fields at: %s:%d .. %d vs %d\n",
+        //         bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, line->n_allele-1);
+        //     return -1;
+        // }
+        // else if ( vlen==BCF_VL_R && nfound!=line->n_allele )
+        // {
+        //     fprintf(stderr, "INFO/%s is Number=R in header. Unexpected number of fields at: %s:%d .. %d vs %d\n",
+        //         bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, line->n_allele);
+        //     return -1;
+        // }
+        // else if ( vlen==BCF_VL_FIXED && nfound!=bcf_hdr_id2number(args->hdr,BCF_HL_INFO,info->key) )
+        // {
+        //     fprintf(stderr, "INFO/%s is Number=%d in header, but %d fields found at: %s:%d\n",
+        //         bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_hdr_id2number(args->hdr,BCF_HL_INFO,info->key), nfound, bcf_seqname(args->hdr,line), line->pos+1);
+        //     return -1;
+        // }
+        // else if ( vlen==BCF_VL_G )
+        // {
+        //     // TODO: guess ploidy based on length of GT -> fill in expected number for G
+        //     int ng = line->n_allele*(line->n_allele + 1)/2;
+        //     if (nfound!=ng && nfound!=line->n_allele) { // diploid or haploid for the moment
+        //         fprintf(stderr, "INFO/%s is Number=G in header. Unexpected number of fields at: %s:%d .. %d vs %d\n",
+        //             bcf_hdr_int2id(args->hdr,BCF_DT_ID,info->key), bcf_seqname(args->hdr,line), line->pos+1, nfound, ng);
+        //         return -1;
+        //     }
+        // }
     }
 
     // check REF matches reference fasta
@@ -158,14 +233,14 @@ int vcf_validate(args_t *args, bcf1_t *line)
         char *ref = line->d.allele[0];
         int ref_len = strlen(ref);
         int fai_ref_len;
-        char *fai_ref = faidx_fetch_seq(args->faidx, bcf_seqname(args->hdr,line), line->pos, line->pos+1, &fai_ref_len);
+        char *fai_ref = faidx_fetch_seq(args->faidx, bcf_seqname(args->hdr,line), line->pos, line->pos+ref_len-1, &fai_ref_len);
         for (i=0; i<fai_ref_len; i++)
             if ( (int)fai_ref[i]>96 ) fai_ref[i] -= 32;
 
         for (i=0; i<fai_ref_len && i<ref_len; i++)
             if ( ref[i] != fai_ref[i] && ref[i] - 32 != fai_ref[i] )
             {
-                fprintf(stderr, "The reference sequence differs at: %s:%d+%d .. %c vs %c\n", bcf_seqname(args->hdr,line), line->pos+1, i, ref[i], fai_ref[i]);        
+                fprintf(stderr, "The reference sequence differs at: %s:%d+%d .. %c vs %c\n", bcf_seqname(args->hdr,line), line->pos+1, i, ref[i], fai_ref[i]);
                 return -1;
             }
     }
@@ -223,7 +298,7 @@ int main_vcfvalidate(int argc, char *argv[])
     args->files   = bcf_sr_init();
     int targets_is_file = 0, regions_is_file = 0;
 
-    static struct option loptions[] = 
+    static struct option loptions[] =
     {
         {"check-counts",0,0,'c'},
         {"fasta-ref",1,0,'f'},
@@ -274,7 +349,7 @@ int main_vcfvalidate(int argc, char *argv[])
     }
 
     if ( !bcf_sr_add_reader(args->files, fname) ) error("Failed to open or the file not indexed: %s\n", fname);
-    
+
     int exit_status = 0;
     args->hdr = args->files->readers[0].header;
     while ( bcf_sr_next_line(args->files) )
